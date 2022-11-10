9147619ffabf593d4d83d96f6e11b6713c10e984
Eman Abu-Ali <emanabuali03@gmail.com>
2022-11-09 23:37:40 -0600

    Search friends feature implemented

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:mymuse/screens/profile_screen.dart';
 import 'package:mymuse/screens/profile_screen.dart';
+import 'package:mymuse/screens/search_screen.dart';
 
 import '../screens/homePage.dart';
 
 
 const homeScreenItems = [
   homePage(),
-  Text('search'),
+  SearchScreen(),
   //Text('nothing'),
   MonthlyBreakdown(),
 ];
